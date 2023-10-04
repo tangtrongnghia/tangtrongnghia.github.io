b522af5b2364895a8010eec1d5c0abbd508468a4
tangtrongnghia <59104248+tangtrongnghia@users.noreply.github.com>
2023-10-04 15:29:23 +0000

    Nghia vua commit vao luc Wed Oct  4 15:29:23 UTC 2023

--- ahihi.bat
+++ ahihi.bat
@@ -1,4 +1,4 @@
-echo -e "\033[0;31m Cần cù thì bù siêng năng, không cần cù thì mình chơi chiêu"
+echo -e "\033[1;31m Cần cù thì bù siêng năng, không cần cù thì mình chơi chiêu :v \033[0m"
 
 git add .
 git commit -m "Nghia vua commit vao luc `date`"
