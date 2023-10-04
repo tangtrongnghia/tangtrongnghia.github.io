53f9c13f1bed5d20fe593baf93a82b6e72252cc4
tangtrongnghia <59104248+tangtrongnghia@users.noreply.github.com>
2023-10-04 07:42:50 +0000

    Nghia vua commit vao luc Wed Oct  4 07:42:50 UTC 2023

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const MSG = 'Nghĩa có cây ☂ ...'
 console.log(`%c ${MSG}`, 'padding: 0.3rem 1.5rem; font-family: Roboto; font-size: 2em; line-height: 1.4em; color: white; background-color: #FF3CAC; background-image: linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%);');
+
+
+function showNotification() {
+    if(window.Notification && Notification.permission !== "denied") {
+        Notification.requestPermission(function(status) {  // status is "granted", if accepted by user
+            var n = new Notification('Nghĩa thông báo!', { 
+                body: 'Nghĩa nội dung !!',
+                icon: '/images/nghia-192x192.png'
+            }); 
+        });
+    }
+}
