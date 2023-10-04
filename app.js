c899f9da1317eb12ba5e138b0b592d67687c7b56
tangtrongnghia <59104248+tangtrongnghia@users.noreply.github.com>
2023-10-04 17:19:30 +0000

    Nghia vua commit vao luc Wed Oct  4 17:19:30 UTC 2023

---
+++ app.js
@@ -0,0 +1,39 @@
+const APP = {
+    SW: null,
+    init: function () {
+        if ('serviceWorker' in navigator) {
+            // 1. Đăng ký serviceWorker
+            navigator.serviceWorker.register('/sw.js', {
+                scope: '/',
+            }).then(function (registration) {
+                APP.SW = registration.installing || registration.waiting || registration.active
+
+                console.log('ServiceWorker registration successful with scope: ', registration.scope)
+            }, function (err) {
+                console.log('ServiceWorker registration failed: ', err)
+            })
+
+            // 2. Kiểm tra page hiện tại có ServiceWorker không
+            if (navigator.serviceWorker.controller) {
+                console.log('We have a ServiceWorker installed')
+            }
+
+            // 3. Bắt sự kiện cập nhật hoặc đăng ký mới một ServiceWorker
+            navigator.serviceWorker.oncontrollerchange = (e) => {
+                console.log('New ServiceWorker activated')
+            }
+
+            // 4. Remove/Unregister ServiceWorker
+            navigator.serviceWorker.getRegistration().then((regs) => {
+                for (const reg of regs) {
+                    reg.unregister().then(isUnreg => console.log(isUnreg))
+                }
+            })
+
+            // 5. Lắng nghe message từ SW
+            // Cái này từ từ =))
+        } else {
+            console.log('ServiceWorker are not supported')
+        }
+    },
+}
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'mycache_v2';
-const urlsToCache = ['/style.css', '/main.js'];
+const urlsToCache = ['/style.css', '/plapla.js'];
 
 self.addEventListener('install', function (event) {
   // Perform install steps
