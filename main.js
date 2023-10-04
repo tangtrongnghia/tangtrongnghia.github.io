6240b33210979ccd8d6555bb0f8ace24dc3fd239
tangtrongnghia <59104248+tangtrongnghia@users.noreply.github.com>
2023-10-04 06:25:54 +0000

    Nghia vua commit vao luc Wed Oct  4 06:25:54 UTC 2023

--- main.js
+++ main.js
@@ -1,2 +1,1 @@
-console.log('%c Nghĩa', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
-console.log('%c Nghĩa', 'padding: 0.3rem 1.5rem; font-family: Roboto; font-size: 1.2em; line-height: 1.4em; color: white; background-color: #FF3CAC; background-image: linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%);');
+console.log('%c Nghĩa có cây ☂', 'padding: 0.3rem 1.5rem; font-family: Roboto; font-size: 1.2em; line-height: 1.4em; color: white; background-color: #FF3CAC; background-image: linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%);');
