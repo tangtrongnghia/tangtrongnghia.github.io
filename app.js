8b0714c2943f213bdd52d4a0ddfe7052acd4360d
tangtrongnghia <59104248+tangtrongnghia@users.noreply.github.com>
2023-10-29 07:08:40 +0000

    Nghia vua commit vao luc Sun Oct 29 07:08:40 UTC 2023

--- app.js
+++ app.js
@@ -40,3 +40,22 @@ const APP = {
 
 // init sau khi DOM đã được load xong
 document.addEventListener('DOMContentLoaded', APP.init)
+
+// Install button
+let deferredPrompt
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    deferredPrompt = e
+})
+
+const installApp = document.getElementById('installApp')
+
+installApp.addEventListener('click', async () => {
+    if (deferredPrompt !== null) {
+        deferredPrompt.prompt()
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            deferredPrompt = null
+        }
+    }
+})
